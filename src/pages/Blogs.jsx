aa2fcc447a65632a33483095d44ee64f918f6102
sufian015 <ab.sufian1997@gmail.com>
2023-07-30 10:10:46 +0600

    first commit

---
+++ src/pages/Blogs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Blogs = () => {
+  return (
+    <div>
+        <h1>This is Blog page</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident, placeat ipsum iure non eaque recusandae ratione fugiat? Sint iusto error qui alias. Amet quae dignissimos accusamus aliquid quos consequatur exercitationem rerum voluptatibus? Totam voluptatem non dignissimos, consectetur error at corrupti?</p>
+    </div>
+    
+  )
+}
+
+export default Blogs
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident, placeat ipsum iure non eaque recusandae ratione fugiat? Sint iusto error qui alias. Amet quae dignissimos accusamus aliquid quos consequatur exercitationem rerum voluptatibus? Totam voluptatem non dignissimos, consectetur error at corrupti?</p>
+    </div>
+  )
+}
+
+export default Home
